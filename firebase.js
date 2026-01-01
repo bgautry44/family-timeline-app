df47e7c1680e95013808d90507d2c4c5cf454368
bgautry44 <pathwaytopurpose11@gmail.com>
2026-01-01 03:31:57 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 // firebase.js (family-timeline-app)
 
 const firebaseConfig = {
-  apiKey: "PASTE_YOURS",
-  authDomain: "PASTE_YOURS",
-  projectId: "PASTE_YOURS",
-  storageBucket: "PASTE_YOURS",
-  messagingSenderId: "PASTE_YOURS",
-  appId: "PASTE_YOURS"
+  apiKey: "AIzaSyAkk1WBSSYUM-hd48D3jLwYWkz_zambftk",
+  authDomain: "family-timeline-e51c5.firebaseapp.com",
+  projectId: "family-timeline-e51c5",
+  storageBucket: "family-timeline-e51c5.firebasestorage.app",
+  messagingSenderId: "738169039790",
+  appId: "1:738169039790:web:5709df6e779662e95af63f"
 };
 
 firebase.initializeApp(firebaseConfig);
