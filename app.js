d77a3618196461be2b4745ba844a5ff0643281be
bgautry44 <pathwaytopurpose11@gmail.com>
2026-01-11 04:30:16 -0600

    Update app.js

--- app.js
+++ app.js
@@ -802,7 +802,7 @@ function fmtEventDate(d) {
 
     const email = user.email || "";
     await ref.set({
-      role: "admin",
+      role: "member",
       email,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       updatedAt: firebase.firestore.FieldValue.serverTimestamp()
